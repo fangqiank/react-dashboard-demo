6e35c5031f50be784b03ce7f960bff4018bac47a
fangqiank <fangqiank@yahoo.com>
2022-06-28 09:08:52 +0800

    commit on Jun.28, 2022

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -8,13 +8,24 @@ import {Header} from '../components/Header'
 export const Calendar = () => {
 	const [scheduleObj, setScheduleObj] = React.useState(null)
 
+	const PropertyPane = props => (
+		<div className='mt-5'>
+			{props.children}
+		</div>
+	)
+
+	const handleChange = (e) => {
+		scheduleObj.selectedDate = e.value
+		scheduleObj.dataBind()
+	}
+
 	return (
 		<div className='m-2 md:m-10 mt-24 p-2 md:p1-0 bg-white rounded-3xl'>
 			<Header category='App' title='Calendar' />
 			<ScheduleComponent
 			 height='650px'
-			//  ref={schedule => setScheduleObj(schedule)}
-			 selectedDate={new Date(2022, 1, 1)}
+			 ref={schedule => setScheduleObj(schedule)}
+			 selectedDate={new Date(2021, 0, 10)}
 			 eventSettings={{dataSource: scheduleData}}
 			//  dragStart={arg => arg.navigation.enable = true}
 			>
@@ -27,6 +38,26 @@ export const Calendar = () => {
 		
 				<Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
 			</ScheduleComponent>
+
+			<PropertyPane>
+				<table
+          style={{ width: '100%', background: 'white' }}
+        >
+          <tbody>
+            <tr style={{ height: '50px' }}>
+              <td style={{ width: '100%' }}>
+                <DatePickerComponent
+                  value={new Date(2021, 0, 10)}
+                  showClearButton={false}
+                  placeholder="Current Date"
+                  floatLabelType="Always"
+                  change={handleChange}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+			</PropertyPane>
 		</div>
 	)
 }
