276b727dd5c9d517eb4627f747ec799bb08f4ec4
fangqiank <fangqiank@yahoo.com>
2022-06-28 08:53:55 +0800

    commit on Jun.28, 2022

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import {ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule'
+import {DatePickerComponent} from '@syncfusion/ej2-react-calendars'
+
+import {scheduleData} from '../data/dummy'
+import {Header} from '../components/Header'
 
 export const Calendar = () => {
+	const [scheduleObj, setScheduleObj] = React.useState(null)
+
 	return (
-		<div>Calendar</div>
+		<div className='m-2 md:m-10 mt-24 p-2 md:p1-0 bg-white rounded-3xl'>
+			<Header category='App' title='Calendar' />
+			<ScheduleComponent
+			 height='650px'
+			//  ref={schedule => setScheduleObj(schedule)}
+			 selectedDate={new Date(2022, 1, 1)}
+			 eventSettings={{dataSource: scheduleData}}
+			//  dragStart={arg => arg.navigation.enable = true}
+			>
+				<ViewsDirective>
+					{['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map(item => (
+						<ViewDirective key={item} option={item}/>
+					))}
+
+				</ViewsDirective>
+		
+				<Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
+			</ScheduleComponent>
+		</div>
 	)
 }
