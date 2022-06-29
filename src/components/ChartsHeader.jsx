d6b62eed0ba8aa756d12354eba1286d7a3c8343b
fangqiank <fangqiank@yahoo.com>
2022-06-29 09:01:04 +0800

    Commit on Jun.29, 2022

--- src/components/ChartsHeader.jsx
+++ src/components/ChartsHeader.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export const ChartsHeader = () => {
+export const ChartsHeader = ({category, title}) => {
 	return (
-		<div>ChartsHeader</div>
+		<div className="mb-10">
+			<div>
+				<p className='text-lg text-gray-400'>Chart</p>
+				<p className='text-3xl font-extrabold tracking-tighter dark:text-gray-200 text-slate-900'>{category}</p>
+			</div>
+			<p className='text-center dark:text-gray-200 text-xl mb-2 mt-3'>{title}</p>
+		</div>
 	)
 }
--- src/pages/charts/Area.jsx
+++ src/pages/charts/Area.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import {ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, SplineAreaSeries,Legend} from '@syncfusion/ej2-react-charts'
+import {ChartHeader} from '../../components/ChartHeader'
+import {areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis} from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 export const Area = () => {
+	const {currentMode} = useStateContext()
+
 	return (
-		<div>Area</div>
+		<div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+			<ChartHeader 
+				category='Area' 
+				title='Inflation Rate in Percentage' 
+			/>
+			<div className='w-full'>
+				<ChartComponent 
+					id='charts' 
+					primaryXAxis={areaPrimaryXAxis}
+					primaryYAxis={areaPrimaryYAxis}
+					chartArea={{border:{width:0}}}
+					background={currentMode === 'dark' ? '#33373e' : '#fff'}
+					legendSettings={{background: '#fff'}}
+				>
+					<SeriesCollectionDirective>
+						{areaCustomSeries.map((item, ind) => (
+							<SeriesDirective
+								key={ind}
+								{...item} 
+							/>						
+						))}
+					</SeriesCollectionDirective>
+
+					<Inject services={[SplineAreaSeries, DateTime, Legend]} />
+				</ChartComponent>
+			</div>
+		</div>
 	)
 }
