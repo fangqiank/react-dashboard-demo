73617940543cd6bdd6b234f156d79e77ec5e4bab
fangqiank <fangqiank@yahoo.com>
2022-06-29 09:20:52 +0800

    Commit on Jun.29, 2022

--- src/pages/charts/Bar.jsx
+++ src/pages/charts/Bar.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import {ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel} from '@syncfusion/ej2-react-charts'
+import {barCustomSeries, barPrimaryXAxis, barPrimaryYAxis} from '../../data/dummy'
+import { ChartsHeader } from '../../components/ChartsHeader'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 export const Bar = () => {
+	const {currentMode} = useStateContext()
+
 	return (
-		<div>Bar</div>
+		<div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+			<ChartsHeader
+				category='Bar'
+				title='Olympic Medal Counts' 
+			/>
+			<div className='w-full'>
+				<ChartComponent 
+					id='charts'
+					primaryXAxis={barPrimaryXAxis}
+					primaryYAxis={barPrimaryYAxis}
+					chartArea={{border: {width: 0}}}
+					tooltip={{enable: true}}
+					background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          legendSettings={{ background: 'white' }}
+				>
+					<SeriesCollectionDirective>
+						{barCustomSeries.map((data, ind) => (
+							<SeriesDirective 
+								key={ind}
+								{...data} 
+							/>
+						))}
+					</SeriesCollectionDirective>
+
+					<Inject services={[ColumnSeries, DataLabel, Category, Legend, Tooltip]} />
+				</ChartComponent>
+			</div>
+		</div>
 	)
 }
