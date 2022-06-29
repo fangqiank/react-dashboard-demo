0f724dc0fad95e04b27d2689ef3b8eb89651f586
fangqiank <fangqiank@yahoo.com>
2022-06-29 09:03:51 +0800

    Commit on Jun.29, 2022

--- src/pages/charts/Area.jsx
+++ src/pages/charts/Area.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, SplineAreaSeries,Legend} from '@syncfusion/ej2-react-charts'
-import {ChartHeader} from '../../components/ChartHeader'
+import {ChartsHeader} from '../../components/ChartsHeader'
 import {areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis} from '../../data/dummy'
 import { useStateContext } from '../../contexts/ContextProvider'
 
@@ -9,7 +9,7 @@ export const Area = () => {
 
 	return (
 		<div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-			<ChartHeader 
+			<ChartsHeader 
 				category='Area' 
 				title='Inflation Rate in Percentage' 
 			/>
