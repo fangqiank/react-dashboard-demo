43537a882ba15f55e134069a7405a5f012333327
fangqiank <fangqiank@yahoo.com>
2022-06-29 08:02:38 +0800

    Commit on Jun.29, 2022

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { format } from 'date-fns'
 
 export const Footer = () => {
 	return (
-		<div>Footer</div>
+		<div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © {format(Date.now(), 'yyyy')} - All Rights Reserved
+    </p>
+  </div>
 	)
 }
